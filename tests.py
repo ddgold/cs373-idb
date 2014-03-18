3d553b378f6786d27890774665d16ceeaffba3fe
ncho91 <noahcho1991@gmail.com>
2014-03-17 22:08:37 -0500

    started writing tests, got imports to work

--- tests.py
+++ tests.py
@@ -1,1 +1,13 @@
- 
+from django.test import TestCase
+from urllib2 import Request, urlopen
+from json import dumps
+
+class test_API(TestCase) :
+
+	# ---
+	# get
+	# ---
+	def test_API_get_game_response(self) :
+		request = Request("http://vgdb.apiary.io/developers")
+		response = urlopen(request)
+		self.assertTrue(True)
