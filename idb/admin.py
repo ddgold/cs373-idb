a2e9f237eeb270db3aa2cc7e673bd5ce0565eef3
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2014-03-18 19:39:12 -0500

    Rename models.py in attempt to make it work on Heroku

--- idb/admin.py
+++ idb/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from idb.models import Game, Platform, Developer
+from idb.Models import Game, Platform, Developer
 
 admin.site.register(Game)
 admin.site.register(Platform)
 admin.site.register(Developer)
+
