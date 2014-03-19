0cb21dc0651bd34d8a5fea2240f1bb80a6ed7031
Doug <ddg625@cs.utexas.edu>
2014-03-19 18:48:22 -0500

    Updated tests to look at json. Need to look at imports for python2/3.  Fixed #22.

--- tests.py
+++ tests.py
@@ -1,157 +1,284 @@
+#!/usr/bin/env python3
+
 from django.test import TestCase
-from urllib2 import Request, urlopen
+from urllib2 import Request, urlopen #python2
+#from urllib.request import urlopen, Request #python3
 from json import dumps, loads
+
+
+
+
+platinum_games = {
+	"name": "Platinum Games",
+	"date_founded": "8-1-2006",
+	"num_employees": 120,
+	"status": "Active",
+	"image_link": "http://upload.wikimedia.org/wikipedia/en/9/9e/PlatinumGames.png",
+	"map_link": "https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=1-1-30+Oyodo-naka,+Kita-ku,+Osaka,+531-6108&amp;aq=&amp;sll=34.704426,135.485297&amp;sspn=0.004397,0.008256&amp;ie=UTF8&amp;hq=&amp;hnear=1+Chome-1-30+%C5%8Cyodonaka,+Kita-ku,+%C5%8Csaka-shi,+%C5%8Csaka-fu,+Japan&amp;ll=34.70533,135.489699&amp;spn=0.035033,0.066047&amp;t=m&amp;z=14&amp;output=embed"
+ }
+
+the_wonderful_101 = {
+	"title": "The Wonderful 101",
+	"release_date": "09-15-2013",
+	"publisher": "Nintendo",
+	"ESRB_rating": "T",
+	"genre": "Action",
+	"image_link": "http://upload.wikimedia.org/wikipedia/en/e/ee/Wonderful_101_box_artwork.jpg",
+	"youtube_link": "//www.youtube.com/embed/bNefE89DZds"
+}
+
+wii_u = {
+	"name": "Wii U",
+	"manufacturer": "Nintendo",
+	"release_date": "11-18-2012",
+	"generation": "Physical (disks) and digital",
+	"media_format": 8,
+	"image_link": "http://timenerdworld.files.wordpress.com/2012/11/top10_gadgets_wiiu.jpg?w=480&h=320&crop=1",
+	"youtube_link": "//www.youtube.com/embed/qhlDHeCT-Q8",
+	"twitter_link": "https://twitter.com/wiiudaily"
+}
+
+
+
+
 
 class test_API(TestCase) :
 
-	# ---
-	# get
-	# ---
-	def test_API_get_developers_response(self) :
-		request = Request("http://vgdb.apiary.io/developers")
+	# ----------
+	# Developers
+	# ----------
+	def test_API_get_developers(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/developers")
 		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_developers_object_response(self) :
-		request = Request("http://vgdb.apiary.io/developers/platinum_games")
-		response_body = urlopen(request).read().decode("utf-8")
-		response = loads(response_body)
-		print(response_body)
-		#self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_developers_object_games_response(self) :
-		request = Request("http://vgdb.apiary.io/developers/platinum_games/games")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_developers_object_platforms_response(self) :
-		request = Request("http://vgdb.apiary.io/developers/platinum_games/platforms")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_games_response(self) :
-		request = Request("http://vgdb.apiary.io/games")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_games_object_response(self) :
-		request = Request("http://vgdb.apiary.io/games/the_wonderful_101")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_games_object_developers_response(self) :
-		request = Request("http://vgdb.apiary.io/games/the_wonderful_101/developers")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_games_object_platforms_response(self) :
-		request = Request("http://vgdb.apiary.io/games/the_wonderful_101/platforms")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_platforms_response(self) :
-		request = Request("http://vgdb.apiary.io/platforms")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_platforms_object_response(self) :
-		request = Request("http://vgdb.apiary.io/platforms/wii_u")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_platforms_object_developers_response(self) :
-		request = Request("http://vgdb.apiary.io/platforms/wii_u/developers")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	def test_API_get_platforms_object_games_response(self) :
-		request = Request("http://vgdb.apiary.io/platforms/wii_u/games")
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 200)
-
-	# ---
-	# post
-	# ---
-
-	def test_API_post_developers_response(self):
-
-		values = dumps([{
-		    'name': 'Platinum Games',
-		    'date_founded': 2006,
-		    'num_employees': 120,
-		    'status': 'Active'
-		}])
-
-		headers = {"Content-Type": "application/json"}
-		request = Request("http://vgdb.apiary.io/developers", data=values, headers=headers)
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 201)
-
-	def test_API_post_platforms_response(self):
-		values = dumps([{
-		    'name': 'Wii U',
-		    'manufacturer': 'Nintendo',
-		    'release_date': 11-18-2012,
-		    'generation': 'Physical (disks) and digital',
-		    'media_format': 8
-		}])
-		headers = {"Content-Type": "application/json"}
-		request = Request("http://vgdb.apiary.io/platforms", data=values, headers=headers)
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 201)
-
-	def test_API_post_games_response(self):
-		values = dumps([{
-		    'title': 'The Wonderful 101',
-		    'release_date': 9-15-2013,
-		    'publisher': 'Nintendo',
-		    'ESRB_rating': 'T',
-		    'genre': 'Action'
-		}])
-		headers = {"Content-Type": "application/json"}
-		request = Request("http://vgdb.apiary.io/games", data=values, headers=headers)
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 201)
-
-	# ---
-	# put
-	# ---
-	def test_API_put_platforms(self):
-		request = Request("http://vgdb.apiary.io/platforms/wii_u")
-		request.get_method = lambda: 'PUT'
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 204)
-
-	def test_API_put_game(self):
-		request = Request("http://vgdb.apiary.io/game/the_wonderful_101")
-		request.get_method = lambda: 'PUT'
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 204)
-
-	def test_API_put_developer(self):
-		request = Request("http://vgdb.apiary.io/developers/platinum_games")
-		request.get_method = lambda: 'PUT'
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 204)
-
-	# ---
-	# delete
-	# ---
-
-	def test_API_delete_platform(self):
-		request = Request("http://vgdb.apiary.io/platforms/wii_u")
-		request.get_method = lambda: 'DELETE'
-		response = urlopen(request)
-		self.assertTrue(response.getcode() == 204)
 		
-	def test_API_delete_game(self):
-		request = Request("http://vgdb.apiary.io/platforms/wonderful_101")
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [{"name": platinum_games["name"]}])
+
+	def test_API_post_developers(self) :
+		values = dumps(platinum_games).encode("utf-8")
+		headers = {"Content-Type": "application/json"}
+		request = Request("http://vgdb.apiary-mock.com/api/developers", data=values, headers=headers)
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 201)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == {"name": platinum_games["name"]})
+
+	def test_API_get_developer(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/developers/platinum_games")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == platinum_games)
+
+	def test_API_put_developer(self) :
+		values = dumps(platinum_games).encode("utf-8")
+		headers = {"Content-Type": "application/json"}
+		request = Request("http://vgdb.apiary-mock.com/api/developers/platinum_games", data=values, headers=headers)
+		request.get_method = lambda: 'PUT'
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 204)
+
+		response_content = response.read()
+		self.assertTrue(response_content == "")
+
+	def test_API_delete_developer(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/developers/platinum_games")
 		request.get_method = lambda: 'DELETE'
 		response = urlopen(request)
-		self.assertTrue(response.getcode() == 204)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 204)
+
+		response_content = response.read()
+		self.assertTrue(response_content == "")
+
+	def test_API_get_developer_games(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/developers/platinum_games/games")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [the_wonderful_101])
+
+	def test_API_get_developer_platforms(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/developers/platinum_games/platforms")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [wii_u])
+
+
+	# --------
+	# Platform
+	# --------
+	def test_API_get_platforms(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/platforms")
+		response = urlopen(request)
 		
-	def test_API_delete_developer(self):
-		request = Request("http://vgdb.apiary.io/platforms/platinum_games")
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [{"name": wii_u["name"]}])
+
+	def test_API_post_platforms(self) :
+		values = dumps(wii_u).encode("utf-8")
+		headers = {"Content-Type": "application/json"}
+		request = Request("http://vgdb.apiary-mock.com/api/platforms", data=values, headers=headers)
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 201)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == {"name": wii_u["name"]})
+
+	def test_API_get_platform(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/platforms/wii_u")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == wii_u)
+
+	def test_API_put_platform(self) :
+		values = dumps(wii_u).encode("utf-8")
+		headers = {"Content-Type": "application/json"}
+		request = Request("http://vgdb.apiary-mock.com/api/platforms/wii_u", data=values, headers=headers)
+		request.get_method = lambda: 'PUT'
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 204)
+
+		response_content = response.read()
+		self.assertTrue(response_content == "")
+
+	def test_API_delete_platform(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/platforms/wii_u")
 		request.get_method = lambda: 'DELETE'
 		response = urlopen(request)
-		self.assertTrue(response.getcode() == 204)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 204)
+
+		response_content = response.read()
+		self.assertTrue(response_content == "")
+
+	def test_API_get_platform_developers(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/platforms/wii_u/developers")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [platinum_games])
+
+	def test_API_get_platform_games(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/platforms/wii_u/games")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [the_wonderful_101])
+
+	
+	# ----
+	# Game
+	# ----
+	def test_API_get_games(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/games")
+		response = urlopen(request)
+		
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [{"title": the_wonderful_101["title"]}])
+
+	def test_API_post_games(self) :
+		values = dumps(the_wonderful_101).encode("utf-8")
+		headers = {"Content-Type": "application/json"}
+		request = Request("http://vgdb.apiary-mock.com/api/games", data=values, headers=headers)
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 201)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == {"title": the_wonderful_101["title"]})
+
+	def test_API_get_game(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/games/the_wonderful_101")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == the_wonderful_101)
+
+	def test_API_put_game(self) :
+		values = dumps(the_wonderful_101).encode("utf-8")
+		headers = {"Content-Type": "application/json"}
+		request = Request("http://vgdb.apiary-mock.com/api/games/the_wonderful_101", data=values, headers=headers)
+		request.get_method = lambda: 'PUT'
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 204)
+
+		response_content = response.read()
+		self.assertTrue(response_content == "")
+
+	def test_API_delete_game(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/games/the_wonderful_101")
+		request.get_method = lambda: 'DELETE'
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 204)
+
+		response_content = response.read()
+		self.assertTrue(response_content == "")
+
+	def test_API_get_game_developers(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/games/the_wonderful_101/developers")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [platinum_games])
+
+	def test_API_get_game_platforms(self) :
+		request = Request("http://vgdb.apiary-mock.com/api/games/the_wonderful_101/platforms")
+		response = urlopen(request)
+
+		response_code = response.getcode()
+		self.assertTrue(response_code == 200)
+
+		response_content = loads(response.read().decode("utf-8"))
+		self.assertTrue(response_content == [wii_u])
