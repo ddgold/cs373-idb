0ed76ee13e6188971fd946399e80f121a90f016e
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2014-03-20 17:13:54 -0500

    Make Models file lowercase

--- idb/admin.py
+++ idb/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from idb.Models import Game, Platform, Developer
+from idb.models import Game, Platform, Developer
 
 admin.site.register(Game)
 admin.site.register(Platform)
