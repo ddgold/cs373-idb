bb78bdc3dd4f36c379efa73c76edfb4992370167
Noah Cho <ncho91@cs.utexas.edu>
2014-04-03 00:17:10 -0500

    adding resources.py to be tracked

---
+++ idb/resources.py
@@ -0,0 +1,50 @@
+from tastypie import fields
+from tastypie.authorization import Authorization
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from idb.models import Platform, Developer, Game
+
+class PlatformResource(ModelResource):
+    class Meta:
+        queryset = Platform.objects.all()
+        resource_name = 'platforms'
+        authorization = Authorization()
+        allowed_methods = ['get', 'post', 'put']
+        filtering = {
+            'name': ['exact', 'starstwith', 'endswith', 'contains'],
+            'manufacturer': ['exact', 'starstwith', 'endswith', 'contains'],
+            'release_date': ['exact', 'lt', 'gt', 'lte', 'gte', 'range'],
+            'media_format': ['exact', 'starstwith', 'endswith', 'contains'],
+            'generation': ['exact', 'gt', 'gte', 'lt', 'lte', 'range'],
+        }
+
+
+class DeveloperResource(ModelResource):
+    platforms = fields.ToManyField(PlatformResource, 'platforms')
+    class Meta:
+        queryset = Developer.objects.all()
+        resource_name = 'developers'
+        authorization = Authorization()
+        allowed_methods = ['get', 'post', 'put']
+        filtering = {
+            'name' : ['exact', 'starstwith', 'endswith', 'contains'],
+            'date_founded' :['exact', 'lt', 'lte', 'gt', 'gte', 'range'],
+            'num_employees' : ['exact', 'lt', 'lte', 'gt', 'gte', 'range'],
+            'status' : ['exact'],
+            'address' : ['exact', 'contains', 'startswith', 'endswith'],
+        }
+
+class GameResource(ModelResource):
+    developer = fields.ForeignKey(DeveloperResource, 'developers')
+    platforms = fields.ToManyField(PlatformResource, 'platforms')
+    class Meta:
+        queryset = Game.objects.all()
+        resource_name = 'games'
+        authorization = Authorization()
+        allowed_methods = ['get', 'post', 'put', 'delete']
+        filtering = {
+            'title' : ['exact', 'starstwith', 'endswith', 'contains'],
+            'release_date' : ['exact', 'lt', 'lte', 'gt', 'gte', 'range'],
+            'genre' : ['exact', 'starstwith', 'endswith', 'contains'],
+            'publisher' : ['exact', 'starstwith', 'endswith', 'contains'],
+            'ESRB_rating' : ['exact'],
+        }
